9c9b5b181b8dac146f1b0ad2978d15961f430627
Ayesha <ayesha19765@gmail.com>
2026-07-24 01:17:09 +0530

    feat: add Employee JPA entity

---
+++ src/main/java/com/codingshuttle/TestingApp/entities/Employee.java
@@ -0,0 +1,29 @@
+package com.codingshuttle.TestingApp.entities;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.*;
+import jakarta.persistence.Id;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String name;
+
+    private Long salary;
+}
